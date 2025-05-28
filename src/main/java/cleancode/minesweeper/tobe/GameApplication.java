f6ddeadb633055baec645b4772bcfc70fcac9ada
techhan <devhan9117@gmail.com>
2025-05-29 00:31:09 +0900

    [4-25] SRP: Single Responsibility Principle

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,13 @@
+package cleancode.minesweeper.tobe;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameApplication {
+
+    public static void main(String[] args) {
+        Minesweeper minesweeper = new Minesweeper();
+        minesweeper.run();
+    }
+}
