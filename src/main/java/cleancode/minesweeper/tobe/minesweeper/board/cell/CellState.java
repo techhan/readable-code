a66700d8b12834caf045d2afe711b6d10e9fbbe9
techhan <devhan9117@gmail.com>
2025-06-03 14:09:03 +0900

    [6-41] 기능 유지보수하기(1) - 버그잡기

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
@@ -21,10 +21,6 @@ public class CellState {
         this.isOpened = true;
     }
 
-    public boolean isChecked() {
-        return isFlagged || isOpened;
-    }
-
     public boolean isOpened() {
         return isOpened;
     }
