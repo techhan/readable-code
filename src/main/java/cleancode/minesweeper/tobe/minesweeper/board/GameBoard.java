27c6b8ffd81eb796d1c770ca77c64e6f936006ff
techhan <devhan9117@gmail.com>
2025-06-03 14:33:25 +0900

    [6-42] 기능 유지보수하기(2) - 알고리즘 교체하기

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/GameBoard.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/GameBoard.java
@@ -7,6 +7,7 @@ import cleancode.minesweeper.tobe.minesweeper.board.position.CellPositions;
 import cleancode.minesweeper.tobe.minesweeper.board.position.RelativePosition;
 
 import java.util.List;
+import java.util.Stack;
 
 
 public class GameBoard {
@@ -42,7 +43,8 @@ public class GameBoard {
             return;
         }
 
-        openSurroundedCells(cellPosition);
+        //openSurroundedCells(cellPosition);
+        openSurroundedCells2(cellPosition);
         checkIfGameIsOver();
     }
 
@@ -152,6 +154,38 @@ public class GameBoard {
 
         List<CellPosition> surroundedPositions = calculateSurroundedPosition(cellPosition, getRowSize(), getColSize());
         surroundedPositions.forEach(this::openSurroundedCells);
+    }
+
+    private void openSurroundedCells2(CellPosition cellPosition) {
+        Stack<CellPosition> stack = new Stack<>();
+        stack.push(cellPosition);
+
+        while(!stack.isEmpty()) {
+            openAndPushCellAt(stack);
+        }
+    }
+
+    private void openAndPushCellAt(Stack<CellPosition> stack) {
+        CellPosition currentCellPosition = stack.pop();
+
+        if (isOpenedCell(currentCellPosition)) {
+            return;
+        }
+
+        if (isLandMineCellAt(currentCellPosition)) {
+            return;
+        }
+
+        openOneCellAt(currentCellPosition);
+
+        if (doesCellHaveLandMineCount(currentCellPosition)) {
+            return;
+        }
+
+        List<CellPosition> surroundedPositions = calculateSurroundedPosition(currentCellPosition, getRowSize(), getColSize());
+        for(CellPosition surroundedPosition : surroundedPositions) {
+            stack.push(surroundedPosition);
+        }
     }
 
     private boolean isOpenedCell(CellPosition cellPosition) {
